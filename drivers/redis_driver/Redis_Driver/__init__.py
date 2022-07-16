699eb8956a746ff2afad28e1a2027dc7b268edc8
duftcola-dev <robinviera@hotmail.com>
2022-07-16 23:32:25 +0200

    + redis driver .+ Added basic commands module . + Added client managament module. + Added ACL and user management module.

---
+++ drivers/redis_driver/Redis_Driver/__init__.py
@@ -0,0 +1,6 @@
+from Redis_Driver.utils.driver import Driver
+driver = Driver()
+
+# Pending tasks
+# Implement token creation and lock creation with the command set 
+# Implement pattern time series with command append 
---
+++ drivers/redis_driver/main.py
@@ -0,0 +1,5 @@
+from itertools import dropwhile
+from Redis_Driver import driver
+
+driver.connect(password="root")
+
